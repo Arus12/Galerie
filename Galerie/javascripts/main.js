50c287fda00b61444921c3ff05088feafde56c3a
Jan Černý <74651859+Arus12@users.noreply.github.com>
2022-01-18 17:21:57 +0100

    Add files via upload

---
+++ Galerie/javascripts/main.js
@@ -0,0 +1,11 @@
+const signUpButton = document.getElementById('signup');
+const signInButton = document.getElementById('signin');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
